44583fec6f3c23d40a0ac907baa46d8620269372
Peter van Dijk <peter.van.dijk@powerdns.com>
2020-04-25 21:46:02 +0200

    change to .format()

--- poc/test.py
+++ poc/test.py
@@ -19,20 +19,20 @@ alg = int(sys.argv[1])
 domain = sys.argv[2]
 nsname = sys.argv[3]
 
-print("### got server certificate (PEM):\n%s" % (ssl.get_server_certificate((nsname, 853))),)
+print("### got server certificate (PEM):\n{}".format(ssl.get_server_certificate((nsname, 853))))
 
 s = socket.create_connection((nsname, 853))
 c = ssl.create_default_context()
 c.check_hostname = False
 c.verify_mode = ssl.CERT_NONE
 cs = c.wrap_socket(s, server_hostname=nsname)
-print("### after connecting again, got server certificate (DER):\n%s" % (cs.getpeercert(binary_form=True)),)
+print("### after connecting again, got server certificate (DER):\n{}".format(cs.getpeercert(binary_form=True)))
 
 cert = x509.load_der_x509_certificate(cs.getpeercert(binary_form=True), default_backend())
-print("### pubkey:\n%s" % (cert.public_key()),)
+print("### pubkey:\n{}".format(cert.public_key()))
 spki=cert.public_key().public_bytes(serialization.Encoding.DER, serialization.PublicFormat.SubjectPublicKeyInfo)
-print("### spki:\n%s" % (spki,))
+print("### spki:\n{}".format(spki))
 tohash = dns.name.from_text(domain).to_wire()+b'\x00\x00'+bytes((3,))+bytes((alg,))+spki
 digest = hashlib.sha256(tohash).hexdigest()
-print("### digest for DS:\n%s" % (digest,))
-print("### DS:\n%s IN DS x %d 2 %s" % (domain, alg, digest))
+print("### digest for DS:\n{}".format(digest))
+print("### DS:\n{} IN DS x {} 2 {}".format(domain, alg, digest))
